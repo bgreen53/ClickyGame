6be7b526ad3f38f4dd24de0452039082a1a455d2
Barry Green <Barry.Green@SLFHC.ORG>
2020-06-23 08:30:48 -0700

    Updates

---
+++ precache-manifest.08a1b2b1218f0f5bbd04b32b583ec1d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac99bf251c1bed5aab0103f271148f70",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "00304faeb61f58ba63d9",
+    "url": "/ClickyGame/static/css/main.5ae5bdba.chunk.css"
+  },
+  {
+    "revision": "6ce6f54f724085dc1c01",
+    "url": "/ClickyGame/static/js/2.93eb1781.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyGame/static/js/2.93eb1781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00304faeb61f58ba63d9",
+    "url": "/ClickyGame/static/js/main.6d6a0723.chunk.js"
+  },
+  {
+    "revision": "29cfca7e2c10f6cba413",
+    "url": "/ClickyGame/static/js/runtime-main.5e5c4f3b.js"
+  }
+]);
