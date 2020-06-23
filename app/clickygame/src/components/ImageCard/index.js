60142590bcec10eeaa591b429a9229f89bf30632
bgreen53 <green.barry53@gmail.com>
2020-06-22 23:47:34 -0700

    attempting to restructure correctly

--- app/clickygame/src/components/ImageCard/index.js
+++ app/clickygame/src/components/ImageCard/index.js
@@ -1,18 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
+import images from "./images.json";
 
-function ImageCard(props) {
+class ImageCard extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        images: images,
+        score: 0,
+        topScore: 0
+  
+      }
+    }
+
+handleClick = id => {
+    let newCard = this.state.images.map(image => {
+      if (id === image.id) {
+        if (image.clicked === undefined) {
+          this.setState({
+            score: this.state.score + 1
+          })
+
+          image.clicked = true;
+          if (this.state.score >= this.state.topScore) {
+            this.setState({
+              topScore: this.state.topScore + 1
+            })
+          }
+        }
+        else {
+          this.setState({
+            score: 0
+          })
+        }
+
+      }
+
+
+      return image;
+    })
+    newCard = newCard.sort(() => (
+      Math.random() - 1
+    ))
+    if (this.state.score === 0) {
+      newCard = newCard.map(image => {
+        image.clicked = undefined
+        return image
+      })
+    }
+
+
+    this.setState({
+      images: newCard
+    })
+  }
+
+render() {
     return (
+
+     
+
         <div className="card"  onClick = {()=> {
             props.handleClick(props.id)
         }}>
-            <div className="img-container">
-                <img id= "imgCard" alt="test" src={props.image}  />
-            </div>
+            
+       <img class="card-img-top" id="imgCard" alt="test" src={props.image}  />
+            
             
            
         </div>
+
+          
     );
+}
 }
 
 export default ImageCard;
