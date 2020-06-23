337fa8f4732a9524de9a1a52e1bf794bf88707ca
bgreen53 <green.barry53@gmail.com>
2020-06-23 07:23:33 -0700

    added bootstrap

--- app/clickygame/src/App.js
+++ app/clickygame/src/App.js
@@ -3,21 +3,82 @@ import './App.css';
 import ImageCard from "./components/ImageCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
+import images from "./images.json";
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      images: images,
+      score: 0,
+      topScore: 0
+
+    }
+  }
+  handleClick = id => {
+    let newCard = this.state.images.map(image => {
+      if (id === image.id) {
+        if (image.clicked === undefined) {
+          this.setState({
+            score: this.state.score + 1
+          })
+
+          image.clicked = true;
+          if (this.state.score >= this.state.topScore) {
+            this.setState({
+              topScore: this.state.topScore + 1
+            })
+          }
+        }
+        else {
+          this.setState({
+            score: 0
+          })
+        }
+
+      }
 
 
+      return image;
+    })
+    newCard = newCard.sort(() => (
+      Math.random() - 0.5
+    ))
+    if (this.state.score === 0) {
+      newCard = newCard.map(image => {
+        image.clicked = undefined
+        return image
+      })
+    }
 
 
-   
+    this.setState({
+      images: newCard
+    })
+  }
 
-  function App(){
+  render() {
     return (
       <Wrapper>
         <Title>Clicky Game | Score: {this.state.score} | TopScore: {this.state.topScore} </Title>
-       <ImageCard />
+        <div className="row">
+          {this.state.images.map(image => {
+            return (
+
+              <div className="col-sm-3">
+                <ImageCard handleClick={this.handleClick}
+                  image={image.image} id={image.id} />
+              </div>
+            )
+
+          })}
+        </div>
 
       </Wrapper>
     )
   }
+
+}
 
 
 
--- app/clickygame/src/components/ImageCard/index.js
+++ app/clickygame/src/components/ImageCard/index.js
@@ -1,78 +1,19 @@
 import React, { Component } from "react";
 import "./style.css";
-import images from "./images.json";
-
-class ImageCard extends Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        images: images,
-        score: 0,
-        topScore: 0
-  
-      }
-    }
-
-handleClick = id => {
-    let newCard = this.state.images.map(image => {
-      if (id === image.id) {
-        if (image.clicked === undefined) {
-          this.setState({
-            score: this.state.score + 1
-          })
-
-          image.clicked = true;
-          if (this.state.score >= this.state.topScore) {
-            this.setState({
-              topScore: this.state.topScore + 1
-            })
-          }
-        }
-        else {
-          this.setState({
-            score: 0
-          })
-        }
-
-      }
 
 
-      return image;
-    })
-    newCard = newCard.sort(() => (
-      Math.random() - 1
-    ))
-    if (this.state.score === 0) {
-      newCard = newCard.map(image => {
-        image.clicked = undefined
-        return image
-      })
-    }
-
-
-    this.setState({
-      images: newCard
-    })
-  }
-
-render() {
+function ImageCard(props) {
     return (
-
-     
-
         <div className="card"  onClick = {()=> {
             props.handleClick(props.id)
         }}>
-            
-       <img class="card-img-top" id="imgCard" alt="test" src={props.image}  />
-            
+            <div className="img-container">
+                <img class="img-card-top" id="imgCard" alt="test" src={props.image}  />
+            </div>
             
            
         </div>
-
-          
     );
-}
 }
 
 export default ImageCard;
